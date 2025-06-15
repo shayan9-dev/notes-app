6abb9e909b81b04bb472a9d383e690a006e6dc3f
shayan9-dev <shayanahmed8862@gmail.com>
2025-06-15 21:53:31 +0500

    added model

---
+++ models/note.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, Text
+from db.base import Base
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    content = Column(Text, nullable=True)
