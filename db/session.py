4e231491d66ef43b7842afa92ac4e11607403c9e
shayan9-dev <shayanahmed8862@gmail.com>
2025-06-15 21:52:35 +0500

    added env

--- db/session.py
+++ db/session.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://fast_owner:npg_d4ZLzxj1FPkX@ep-noisy-star-a1mlvxvs-pooler.ap-southeast-1.aws.neon.tech/fast?sslmode=require"
-engine = create_engine(DATABASE_URL)
+from core.config import settings
 
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
