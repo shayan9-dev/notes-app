f80c7343b31bd2076a841d99d019062bc834a23a
shayan9-dev <shayanahmed8862@gmail.com>
2025-06-15 21:56:23 +0500

    created

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-from fastapi import FastAPI, Request
-from pydantic import BaseModel
-from typing import List
+from fastapi import FastAPI
+from db.base import Base
+from db.session import engine
+from api.v1.routes import router as api_router
+from utils.error_handler import validation_exception_handler
+from fastapi.exceptions import RequestValidationError
 
-app = FastAPI()
+app = FastAPI(title="Notes App")
 
-class loginData(BaseModel):
-    email: str
-    password: str
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
 
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-def first():
-    return {"message: hello its working"}
-
-@app.post("/login")
-async def login(data: loginData): 
-    return data
+app.include_router(api_router, prefix="/api/v1")
