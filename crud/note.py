e9e9514f2f42f72b412bd708f19651eda3d4c282
shayan9-dev <shayanahmed8862@gmail.com>
2025-06-18 14:44:50 +0500

    crud functions

--- crud/note.py
+++ crud/note.py
@@ -11,3 +11,21 @@ def create_note(db: Session, note: NoteCreate) -> Note:
 
 def get_notes(db: Session) -> list[Note]:
     return db.query(Note).all()
+
+def update_note(db: Session, note_id: int, title: str, content: str):
+       note = db.query(Note).filter(Note.id == note_id).first()
+       if not note:
+           return None
+       note.title = title
+       note.content = content
+       db.commit()
+       db.refresh(note)
+       return note
+
+def delete_note(db: Session, note_id: int):
+       note = db.query(Note).filter(Note.id == note_id).first()
+       if not note:
+           return None
+       db.delete(note)
+       db.commit()
+       return note
